7b420b44a722f9db173fe5d57dc341a96a91cd71
Viet-Hoang-2005 <tnvhoang2005@gmail.com>
2026-06-27 09:38:23 +0700

    feat(iam): add s3 multipart upload permissions to terraform worker policy

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -24,7 +24,11 @@ data "aws_iam_policy_document" "worker_s3_policy_doc" {
       "s3:PutObject",
       "s3:GetObject",
       "s3:ListBucket",
-      "s3:DeleteObject"
+      "s3:DeleteObject",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListMultipartUploadParts",
+      "s3:AbortMultipartUpload",
+      "s3:GetBucketLocation"
     ]
     resources = [
       var.artifacts_bucket_arn,
